c42c48394759d4ccc225cc1379e391af78aa07f0
pteacher <subfear@mail.ru>
2021-02-04 11:44:17 +0600

    templates added with bootstrap and jquery

---
+++ src/main/java/hello/world/WebController.java
@@ -0,0 +1,18 @@
+package hello.world;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String aboutMe() {
+        return "about";
+    }
+}
--- src/main/java/hello/world/WorldApplication.java
+++ src/main/java/hello/world/WorldApplication.java
@@ -6,24 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class WorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorldApplication.class, args);
     }
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello, COM19!";
-    }
-
-    @ResponseBody
-    @RequestMapping("/about")
-    String about() {
-        return "Done by Ruslan Isaev";
-    }
-
 }
